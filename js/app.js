6ab6f6b8c3bf8c326d43ae403f152eeed9a4b251
paul-ld <paul.lugagnedelpon@gmail.com>
2014-05-12 02:36:14 +0800

    Completed CodeSchool level 2

--- js/app.js
+++ js/app.js
@@ -3,8 +3,36 @@ var App = Ember.Application.create({
   // LOG_TRANSITIONS can be used for debugging
 });
 
-// THE ROUTER + ALL ROUTES COMES HERE
+// THE ROUTER + ALL ROUTES COMES HERE, REFERS TO A TEMPLATE (index, about...)
 App.Router.map(function() {
   this.route('about', { path: '/aboutus' });
   this.route('credits');
 });
+
+
+// THE CONTROLLER IS REQUIRED FOR EACH TEMPLATE! (SCRIPT TYPE TEXT/X-HANDLEBARS)
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'images/ga-logo.png',
+  time: function() {
+    return (new Date()).toDateString()
+  }.property()
+});
+
+App.AboutController = Ember.Controller.extend({
+  today: function() {
+    return (new Date()).toDateString()
+  }.property(),
+  open: function() {
+    if ( (new Date()).getDay() === 6) {
+      return 'Closed';
+    } else {
+      return 'Open';
+    }
+  }.property()
+});
+
+// NB: each template has a default controller that is created 
+// 'behind the scenes'. We only need to create it if we have 
+// to declare some properties for it
+
