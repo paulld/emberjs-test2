43b3c1036031842895255f8ad187546fa92001b1
paul-ld <paul.lugagnedelpon@gmail.com>
2014-05-14 10:54:12 +0800

    codeschool level 5 done. onSale page not filtering like it should

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ var App = Ember.Application.create({
 App.Router.map(function() {
   this.route('about', { path: '/aboutus' });
   this.route('credits');
-  this.resource('works');
+  this.resource('works', function() {
+    this.route('onsale');
+  });
   this.resource('work', { path: '/works/:work_id' });
   this.resource('artists', function() {
     this.resource('artist', { path: '/:artist_id' });
@@ -47,12 +49,19 @@ App.WorksRoute = Ember.Route.extend({
     return this.store.findAll('work');
   }
 });
-App.WorkRoute = Ember.Route.extend({   //NB NOT NECESSARY WITH FIXTURES!!
-  model: function(params) {
-    // console.log(params);
-    return this.store.find('work', params.work_id)
-    // console.log(out.title);
-    // return out;
+// App.WorkRoute = Ember.Route.extend({   //NB NOT NECESSARY WITH FIXTURES!!
+//   model: function(params) {
+//     // console.log(params);
+//     return this.store.find('work', params.work_id)
+//     // console.log(out.title);
+//     // return out;
+//   }
+// });
+
+App.WorksOnsaleRoute = Ember.Route.extend({
+  model: function () {
+    return this.modelFor('works').filterBy('isOnSale');
+    // return this.store.findAll('work').filterBy('isOnSale');
   }
 });
 
@@ -62,11 +71,11 @@ App.ArtistsRoute = Ember.Route.extend({
     return this.store.findAll('artist');
   }
 });
-App.ArtistRoute = Ember.Route.extend({   //NB NOT NECESSARY WITH FIXTURES!!
-  model: function(params) {
-    return this.store.find('artist', params.artist_id)
-  }
-});
+// App.ArtistRoute = Ember.Route.extend({   //NB NOT NECESSARY WITH FIXTURES!!
+//   model: function(params) {
+//     return this.store.find('artist', params.artist_id)
+//   }
+// });
 
 
 
@@ -100,39 +109,35 @@ App.ApplicationController = Ember.ArrayController.extend({
 });
 
 App.IndexController = Ember.ArrayController.extend({
-  // worksCount: function() {
+  // totalWorks: function() {
     // return this.get('length');
     // return this.count1;
   // }.property('length'),
-  worksCount: Ember.computed.alias('length'),
-  artistsCount: 3,
+  totalWorks: Ember.computed.alias('length'),
+  totalArtists: 3,
   logo: 'images/ga-logo.png',
-  workViews: function() {
-    return this.filterBy('workViews', true).slice(0,3);
-    // return this.filter(function(work) {
-    //   return work.get('workViews');
-    // });
-  }.property(),
-  workViewsCount: function() {
-    
-    // var sorting = this.sortBy('workViewsCount').slice(0,3);
-    
+  
+  workViewCount: function() {
+    // return this.sortBy('workViewCount').slice(0,3);
     // return sorting
     // sortAscending: false;
     var workTotal = this.get('length');
-    return this.sortBy('workViewsCount').slice(workTotal-3,workTotal);
-    
+    return this.sortBy('workViewCount').slice(workTotal-3,workTotal);  
     // return this.filter(function(work) {
-    //   return work.get('workViewsCount');
+    //   return work.get('workViewCount');
     // });
-  }.property(),
-  artistViews: function() {
-    
-    // return this.filterBy('artistViews', true).slice(0,3);
+    // return this.filter(function(work) {
+    //   sortAscending: false;
+    //   return this.sortBy('workViewCount').slice(0,3);
+    // });
+  }.property('@each.workViewCount'),
+
+  artistViewCount: function() {
+    // return this.filterBy('artistViewCount', true).slice(0,3);
     return this.filter(function(artist) {
-      return artist.get('artistViews');
+      return artist.get('artistViewCount');
     });
-  }.property()
+  }.property('@each.artistViewCount')
 });
 
 App.AboutController = Ember.Controller.extend({
@@ -155,10 +160,11 @@ App.WorksController = Ember.ArrayController.extend({
   sortAscending: false
 });
 
+// App.WorksOnsaleController = Ember.ObjectController.extend({ });
 App.WorkController = Ember.ObjectController.extend({ });
 
 App.ArtistsController = Ember.ArrayController.extend({
-  sortProperties: ['lastName']
+  sortProperties: ['lastName'],
 });
 
 App.ArtistController = Ember.ObjectController.extend({ });
@@ -173,8 +179,8 @@ App.Work = DS.Model.extend({
   price: DS.attr('number'),
   type: DS.attr('string'),
   description: DS.attr('string'),
-  workViews: DS.attr('boolean'),
-  workViewsCount: DS.attr('number'),
+  isOnSale: DS.attr('boolean'),
+  workViewCount: DS.attr('number'),
   image: DS.attr('string'),
   drawer: DS.belongsTo('artist', {async: true}) 
 });
@@ -188,7 +194,7 @@ App.Artist = DS.Model.extend({
   diedOn: DS.attr('string'),
   description: DS.attr('string'),
   image: DS.attr('string'),
-  artistViews: DS.attr('boolean'),
+  artistViewCount: DS.attr('boolean'),
   works: DS.hasMany('work', {async: true}),
   comments: DS.hasMany('comment', {async: true})    //ASYNC: LOADS IN ADVANCE??
 });
@@ -211,8 +217,8 @@ App.Work.FIXTURES = [    //SPECIFIC TO FIXTURES
     price: 2000000,
     type: 'Painting',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sed massa pellentesque ante tempus feugiat vel vel metus. Cras mi velit, interdum eu viverra cursus, molestie et elit. Pellentesque nec pellentesque turpis. Pellentesque vel nibh tempus, volutpat nisi ac, dictum tellus. Praesent tincidunt purus nec tellus varius, sit amet fermentum nisi sollicitudin. Proin rhoncus pretium nibh, fermentum ultrices augue rhoncus in. Donec id velit ac libero semper tincidunt. Maecenas placerat eget orci at dictum. Mauris in rhoncus mauris, auctor auctor purus. Nunc vitae tincidunt odio, dignissim consectetur nibh. Suspendisse sollicitudin luctus dui in lacinia. Pellentesque mattis arcu mauris, vitae eleifend turpis varius ut. Sed eget augue odio. Donec ac tortor eget nulla rutrum ullamcorper. Curabitur vestibulum tortor elit, vitae pretium eros mollis sed.',
-    workViews: true,
-    workViewsCount: 10,
+    isOnSale: false,
+    workViewCount: 10,
     image: 'images/picasso-guernica.jpg',
     drawer: 100
   },
@@ -222,8 +228,8 @@ App.Work.FIXTURES = [    //SPECIFIC TO FIXTURES
     price: 300000000,
     type: 'Painting',
     description: 'Cras eget molestie lorem. Etiam vehicula viverra tortor quis sodales. Mauris in faucibus augue. Suspendisse potenti. Mauris vitae massa ac nunc convallis dignissim. Donec et lacus at sapien tristique condimentum mattis vitae quam. Aenean arcu ipsum, dapibus ut suscipit eu, ultrices eget sapien. Vestibulum sollicitudin tellus nisl, eu vulputate erat laoreet non. Proin euismod et ipsum id molestie.',
-    workViews: false,
-    workViewsCount: 100,
+    isOnSale: false,
+    workViewCount: 100,
     image: 'images/picasso-avignon.jpg',
     drawer: 100
   },
@@ -233,8 +239,8 @@ App.Work.FIXTURES = [    //SPECIFIC TO FIXTURES
     price: 1000000,
     type: 'Painting',
     description: 'Nulla luctus, ipsum id elementum luctus, ipsum massa consequat nulla, vel venenatis sapien nisl id arcu. Etiam consectetur pretium felis et mattis. Aliquam consectetur mi sit amet adipiscing dictum. Proin enim libero, adipiscing non malesuada a, dictum ac dolor. Vivamus posuere, ligula eget condimentum convallis, leo lacus sodales ligula, sed euismod est ligula sit amet mi. Phasellus tristique porta fringilla. Mauris ullamcorper rhoncus ante, ac varius orci aliquam sed. Vivamus eu turpis vitae quam iaculis feugiat. Suspendisse varius vehicula enim vitae vulputate. Vivamus leo metus, elementum vel dui non, feugiat suscipit lacus. Vivamus varius, enim et viverra accumsan, nunc risus vestibulum nisi, vitae vulputate mauris metus nec massa. Praesent ac volutpat sapien. Donec pretium, justo et pharetra faucibus, augue mauris tincidunt enim, quis condimentum nisi nulla nec tortor. Mauris vehicula consequat nisi, aliquet placerat enim porta non. Proin ante augue, luctus ac auctor eu, vestibulum eget augue.',
-    workViews: false,
-    workViewsCount: 1,
+    isOnSale: false,
+    workViewCount: 1,
     image: 'images/picasso-blue-nude.jpg',
     drawer: 100
   },
@@ -244,8 +250,8 @@ App.Work.FIXTURES = [    //SPECIFIC TO FIXTURES
     price: 20000000,
     type: 'Painting',
     description: 'Proin in consequat sem. Nulla vitae quam et leo volutpat commodo. Maecenas vel nisi purus. Nunc laoreet dolor nec eros imperdiet eleifend. Aliquam sollicitudin nec mauris non tristique. Sed et hendrerit dui. Cras non condimentum purus. Nam volutpat venenatis tortor. Donec a sapien non lectus sagittis iaculis. Morbi ut elit scelerisque, lacinia urna quis, pulvinar libero.',
-    workViews: true,
-    workViewsCount: 5,
+    isOnSale: true,
+    workViewCount: 5,
     image: 'images/dali-grand-masturbateur.png',
     drawer: 101
   },
@@ -255,12 +261,12 @@ App.Work.FIXTURES = [    //SPECIFIC TO FIXTURES
     price: 20000000,
     type: 'Sculpture',
     description: 'Proin in consequat sem. Nulla vitae quam et leo volutpat commodo. Maecenas vel nisi purus. Nunc laoreet dolor nec eros imperdiet eleifend. Aliquam sollicitudin nec mauris non tristique. Sed et hendrerit dui. Cras non condimentum purus. Nam volutpat venenatis tortor. Donec a sapien non lectus sagittis iaculis. Morbi ut elit scelerisque, lacinia urna quis, pulvinar libero.',
-    workViews: true,
-    workViewsCount: 8,
+    isOnSale: false,
+    workViewCount: 8,
     image: 'images/dali-profile-of-time.jpg',
     drawer: 101
-  },
-];
+  }
+]
 
 App.Artist.FIXTURES = [    //SPECIFIC TO FIXTURES
 // App.ARTISTS = [
@@ -274,7 +280,7 @@ App.Artist.FIXTURES = [    //SPECIFIC TO FIXTURES
     diedOn: '8 April 1973 (aged 91)',
     description: 'Pablo Ruiz y Picasso, also known as Pablo Picasso, was a Spanish painter, sculptor, printmaker, ceramicist, stage designer, poet and playwright who spent most of his adult life in France. As one of the greatest and most influential artists of the 20th century, he is known for co-founding the Cubist movement, the invention of constructed sculpture, the co-invention of collage, and for the wide variety of styles that he helped develop and explore. Among his most famous works are the proto-Cubist Les Demoiselles d\'Avignon (1907), and Guernica (1937), a portrayal of the German bombing of Guernica during the Spanish Civil War.',
     image: 'images/artist1.png',
-    artistViews: true,
+    artistViewCount: true,
     comments: [200,201],    //SPECIFIC TO FIXTURES
     works: [1,2,3]
   },
@@ -288,7 +294,7 @@ App.Artist.FIXTURES = [    //SPECIFIC TO FIXTURES
     diedOn: 'January 23, 1989 (aged 84)',
     description: 'Salvador Domingo Felipe Jacinto Dali i Domenech, 1st Marques de Dali de Pubol, known as Salvador Dali, was a prominent Spanish Catalan surrealist painter born in Figueres, Spain. Dali was a skilled draftsman, best known for the striking and bizarre images in his surrealist work. His painterly skills are often attributed to the influence of Renaissance masters. His best-known work, The Persistence of Memory, was completed in August 1931. Dali\'s expansive artistic repertoire included film, sculpture, and photography, in collaboration with a range of artists in a variety of media. Dali attributed his "love of everything that is gilded and excessive, my passion for luxury and my love of oriental clothes" to an "Arab lineage", claiming that his ancestors were descended from the Moors. Dali was highly imaginative, and also enjoyed indulging in unusual and grandiose behavior. His eccentric manner and attention-grabbing public actions sometimes drew more attention than his artwork, to the dismay of those who held his work in high esteem, and to the irritation of his critics.',
     image: 'images/artist2.png',
-    artistViews: false,
+    artistViewCount: false,
     comments: [202],    //SPECIFIC TO FIXTURES
     works: [4,5]
   },
@@ -302,7 +308,7 @@ App.Artist.FIXTURES = [    //SPECIFIC TO FIXTURES
     diedOn: '15 August 1967 (aged 68)',
     description: 'Rene Francois Ghislain Magritte was a Belgian surrealist artist. He became well known for a number of witty and thought-provoking images that fall under the umbrella of surrealism. His work is known for challenging observers\' preconditioned perceptions of reality.',
     image: 'images/artist3.png',
-    artistViews: true,
+    artistViewCount: true,
   },
   {
     id: 103,
@@ -314,9 +320,9 @@ App.Artist.FIXTURES = [    //SPECIFIC TO FIXTURES
     diedOn: 'February 22, 1987 (aged 58)',
     description: 'Andy Warhol was an American artist who was a leading figure in the visual art movement known as pop art. His works explore the relationship between artistic expression, celebrity culture and advertisement that flourished by the 1960s. After a successful career as a commercial illustrator, Warhol became a renowned and sometimes controversial artist. The Andy Warhol Museum in his native city, Pittsburgh, Pennsylvania, holds an extensive permanent collection of art and archives. It is the largest museum in the United States dedicated to a single artist. Warhol\'s art encompassed many forms of media, including hand drawing, painting, printmaking, photography, silk screening, sculpture, film, and music. He was also a pioneer in computer-generated art using Amiga computers that were introduced in 1984, two years before his death. He founded Interview Magazine and was the author of numerous books, including The Philosophy of Andy Warhol and Popism: The Warhol Sixties. He is also notable as a gay man who lived openly as such before the gay liberation movement.',
     image: 'images/artist4.png',
-    artistViews: true,
+    artistViewCount: true,
     comments: [203]    //SPECIFIC TO FIXTURES
-  },
+  }
 ]
 
 App.Comment.FIXTURES = [    //SPECIFIC TO FIXTURES
